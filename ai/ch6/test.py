893c6eaa6a1339401640891abe449b2fa5dcc23d
songzhengxuan <songzhengxuan@gmail.com>
2017-10-29 08:02:53 +0000

    add min-conflict local search algorithm

--- ai/ch6/test.py
+++ ai/ch6/test.py
@@ -1,12 +1,14 @@
 from PIL import Image, ImageDraw
 from random import randint
 import math
+import sys
 
 ColorTable = {}
 ColorTable[0] = (255, 0, 0) 
 ColorTable[1] = (0, 255, 0) 
 ColorTable[2] = (0, 0, 255) 
 ColorTable[3] = (0, 0, 0) 
+ColorTable[4] = (255, 255, 0) 
 
 
 class Point(object):
@@ -18,6 +20,9 @@ class Point(object):
 
     def draw(self, draw):
         draw.ellipse((self.x - Point.__radius, self.y - Point.__radius) + (self.x + Point.__radius, self.y + Point.__radius), ColorTable[self.color])
+    
+    def reset(self):
+        self.color = 0
 
 class Map(object):
     def __init__(self):
@@ -34,6 +39,10 @@ class Map(object):
     def connect(self, p1, p2):
         self.edges.append((p1, p2))
 
+    def reset(self):
+        for p in self.points:
+            p.reset()
+
 def generateRandomMap(width, height, N):
     result = Map()
     for i in range(N):
@@ -43,10 +52,12 @@ def generateRandomMap(width, height, N):
     while True:
         added = False
         for i in range(N):
-            nearest_distance = 10000
+            nearest_distance = sys.maxint
             nearest_point = None
             p1 = result.points[i]
             for j in range(N):
+                if j == i:
+                    continue
                 p2 = result.points[j]
                 valid = True
                 if (p1, p2) in result.edges or (p2, p1) in result.edges:
@@ -80,11 +91,51 @@ def line_length(p1, p2):
 def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
+def minConflict(map, maxColorCount, maxLoop):
+    unassignedPoints = {}
+    for i in range(maxLoop):
+        if isValidMap(map):
+            return
+        if len(unassignedPoints) == 0:
+            unassignedPoints = { p for p in map.points }
+
+        # find a random points in un-assgined points set
+        randomPoint = unassignedPoints.pop()
+        conflictCount = { color : 0 for color in range(1, maxColorCount+1)}
+        for edge in map.edges:
+            h, t = edge
+            if h == randomPoint:
+                if t.color != 0:
+                    conflictCount[t.color] += 1
+            elif t == randomPoint:
+                if h.color != 0:
+                    conflictCount[h.color] += 1
+        minConfilitCount = sys.maxint 
+        minConflictColor = 0
+        for color, conflict in conflictCount.items():
+            if conflict < minConfilitCount:
+                minConfilitCount = conflict
+                minConflictColor = color
+        randomPoint.color = minConflictColor
+    # end of for max loop
+
+def isValidMap(map):
+    for e in map.edges:
+        h, t = e
+        if h.color != 0 and t.color != 0 and h.color != t.color:
+            continue
+        else:
+            return False
+    return True
+
 def main():
     im = Image.new('RGBA', (400, 400), (255, 255, 255, 0))
     draw = ImageDraw.Draw(im)
     #draw.line((0, 0) + im.size, fill=128)
     m = generateRandomMap(400, 400, 6)
+    print(isValidMap(m))
+    minConflict(m, 4, 1000)
+    print(isValidMap(m))
     m.draw(draw)
     im.show()
 
